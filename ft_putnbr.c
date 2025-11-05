0e4ea881112a907a41c811adf39c7a362263c147
Abdulmalik Khalid <khalidalshunnaq@gmail.com>
2025-11-05 10:27:37 +0300

    last edited

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -1,6 +1,13 @@
 #include <unistd.h>
 
-
+void ft_chick(int nb)
+{
+	if (nb == 0)
+	{
+		write(1, "0", 1);
+		return ;
+	}
+}
 void ft_putnbr(int nb)
 {
 	long	nbl; // long is an int with 8 bits ;;;;;;
@@ -9,24 +16,18 @@ void ft_putnbr(int nb)
 
 	nbl = nb;
 	i = 0;
-	if ( nb == 0 )
-	{
-		write(1, "0", 1);
-		return ;
-	}
+	ft_chick(nb);
 	if ( nbl < 0 )
 	{
 	nbl*=-1;
 	write(1, "-", 1);
 	}
-
 	while( nbl % 10 )
 	{
 		c[i] = (nbl % 10 ) + '0';
 		nbl /=10;
 	        i++;	
 	}
-
 	while ( i >= 0 ) // for printing the number .. you start from the last element ( which is the first num's digit ) until 0;;;;;
 	{
 		write (1, &c[i], 1);
@@ -35,11 +36,11 @@ void ft_putnbr(int nb)
 }
 
 
-int main () {
+/*int main () {
 
 
 ft_putnbr(2147483647);
-ft_putnbr(8346);
+ft_putnbr(-2147483);
 
 return 0;
 }
